7b431b98be81b0fcd95386e103dba13aa8fd0fc9
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-19 10:26:07 +0900

    상품페이지의 왼쪽 카테고리 컴포넌트 생성 및 css 추가

---
+++ src/components/Products/ProductsSidebar.jsx
@@ -0,0 +1,9 @@
+import "./ProductsSidebar.css";
+import rocket from "../../assets/rocket.png";
+import LinkWithIcon from "../Navbar/LinkWithIcon";
+
+const ProductsSidebar = () => {
+  return <div>ProductsSidebar</div>;
+};
+
+export default ProductsSidebar;
