e909204a0c5d845b96245a3aebf83d92cc4bd51c
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-19 11:41:25 +0900

    LoginPage 컴포넌트 생성 및 css적용

---
+++ src/components/Authentication/LoginPage.jsx
@@ -0,0 +1,37 @@
+import "./LoginPage.css";
+
+const LoginPage = () => {
+  return (
+    <section className="align_center form_page">
+      <form className="authentication_form">
+        <h2>로그인 폼</h2>
+        <div className="form_inputs">
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              className="form_text_input"
+              placeholder="이메일 입력..."
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              className="form_text_input"
+              placeholder="패스워드"
+            />
+          </div>
+
+          <button type="submit" className="search_button form_submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default LoginPage;
