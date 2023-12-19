fd07d18c72063e0c2c855f128621fc6d7f8b4df0
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-19 11:38:31 +0900

    설명 주석 추가

--- src/components/Common/Table.jsx
+++ src/components/Common/Table.jsx
@@ -1,6 +1,7 @@
 import "./Table.css";
 
 //Table 컴포넌트는 열의 제목들이 배열로 입력되면 제목들을 th태그로 만듬
+//children 자식 태그들을 표시함.
 const Table = ({ headings, children }) => {
   return (
     <table className="common_table">
