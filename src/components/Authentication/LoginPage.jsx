32f757c5f2b187c26a48cac4de663359d338b2dd
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-19 14:09:57 +0900

    submit 시 iinput창 초기화

--- src/components/Authentication/LoginPage.jsx
+++ src/components/Authentication/LoginPage.jsx
@@ -1,16 +1,30 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "./LoginPage.css";
 
 const LoginPage = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  //리액트에서 특정 태그를 선택하는 방법
   const passwordRef = useRef(null);
+
+  //로그인 클릭 시 실행
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //console.log(user);
+    setUser({ email: "", password: "" });
+  };
   return (
     <section className="align_center form_page">
-      <form className="authentication_form">
+      <form onSubmit={handleSubmit} className="authentication_form">
         <h2>로그인 폼</h2>
         <div className="form_inputs">
           <div>
             <label htmlFor="email">Email</label>
             <input
+              onChange={(e) => setUser({ ...user, email: e.target.value })}
+              value={user.email}
               type="email"
               id="email"
               className="form_text_input"
@@ -20,7 +34,9 @@ const LoginPage = () => {
           <div>
             <label htmlFor="password">Password</label>
             <input
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
               type="password"
+              value={user.password}
               ref={passwordRef}
               id="password"
               className="form_text_input"
