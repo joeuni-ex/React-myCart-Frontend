c14ff97c78398ea349897a9422808787e5e50fa0
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-19 10:20:25 +0900

    상품페이지 섹션 구분

---
+++ src/components/Products/ProductPage.jsx
@@ -0,0 +1,12 @@
+import "./ProductPage.css";
+
+const ProductPage = () => {
+  return (
+    <section className="products_page">
+      {/* 왼쪽 카테고리 */}
+      {/* 상품목록 */}
+    </section>
+  );
+};
+
+export default ProductPage;
