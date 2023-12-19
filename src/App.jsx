2b490270ac72b46346027cb828cff2af38cd32dc
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-19 09:45:34 +0900

    HomePage 컴포넌트 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import HomePage from "./components/Home/HomePage";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
@@ -7,7 +8,9 @@ function App() {
       <nav>
         <Navbar />
       </nav>
-      <main>라우팅</main>
+      <main>
+        <HomePage />
+      </main>
     </div>
   );
 }
