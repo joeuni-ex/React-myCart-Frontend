d7f3d807482e0465e3a754895bfb917780688d45
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-19 10:28:18 +0900

    상품페이지 왼쪽 카테고리 컴포넌트 css추가

--- src/components/Products/ProductsSidebar.jsx
+++ src/components/Products/ProductsSidebar.jsx
@@ -3,7 +3,20 @@ import rocket from "../../assets/rocket.png";
 import LinkWithIcon from "../Navbar/LinkWithIcon";
 
 const ProductsSidebar = () => {
-  return <div>ProductsSidebar</div>;
+  return (
+    <aside className="products_sidebar">
+      <h2>카테고리</h2>
+
+      <div className="category_links">
+        <LinkWithIcon
+          title="전자제품"
+          link="products?category=electronics"
+          emoji={rocket}
+          sidebar={true}
+        />
+      </div>
+    </aside>
+  );
 };
 
 export default ProductsSidebar;
