19b0da65130f951dbae8be66a57da09d7b62f414
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-21 11:38:19 +0900

    같은 상품을 장바구니에 여러 번 추가할 경우에 수량은 증가하나 장바구니의 개수는 늘어나지 않도록 업데이트한다.

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,37 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Routing from "./components/Routing/Routing";
 import { jwtDecode } from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { addToCartAPI } from "./services/cartServices";
+
+setAuthToken(localStorage.getItem("token"));
 
 function App() {
   const [user, setUser] = useState(null); //현재 로그인 중인 유저 정보
   const [cart, setCart] = useState([]); //장바구니
+
+  //장바구니에 상품 추가
+  const addToCart = (product, quantity) => {
+    const updatedCart = [...cart]; //기존의 카트
+    //findIndex는 모든 배열아이템과 비교해서 참이 있으면 true를 리턴하고 없으면 -1
+    const productIndex = updatedCart.findIndex(
+      (item) => item.product._id === product._id
+    );
+    if (productIndex === -1) {
+      updatedCart.push({ product: product, quantity: quantity });
+    } else {
+      updatedCart[productIndex].quantity += quantity;
+    }
+    setCart(updatedCart);
+
+    addToCartAPI(product._id, quantity)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  };
 
   //실행 시 로컬 스토리지에서 토큰 가져온다
   useEffect(() => {
@@ -25,12 +52,6 @@ function App() {
       }
     } catch (error) {}
   }, []);
-
-  //장바구니에 상품 추가
-  const addToCart = (product, quantity) => {
-    //기존의 카트 데이터 뒤에 새로운 데이터 추가
-    setCart([...cart, { product, quantity }]);
-  };
 
   //console.log(user);
   return (
