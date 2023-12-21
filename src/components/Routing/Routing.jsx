2f71b0ffa15e4e0aaf052b589e234ada1a21dd62
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-21 10:37:43 +0900

    로그아웃 기능 추가

--- src/components/Routing/Routing.jsx
+++ src/components/Routing/Routing.jsx
@@ -7,6 +7,7 @@ import SignupPage from "../Authentication/SignupPage";
 import LoginPage from "../Authentication/LoginPage";
 import CartPage from "../Cart/CartPage";
 import MyOrderPage from "../MyOrderPage/MyOrderPage";
+import Logout from "../Authentication/Logout";
 
 const Routing = () => {
   return (
@@ -18,6 +19,7 @@ const Routing = () => {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/cart" element={<CartPage />} />
       <Route path="/myorders" element={<MyOrderPage />} />
+      <Route path="/logout" element={<Logout />} />
     </Routes>
   );
 };
