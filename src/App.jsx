6ffcb5a65a62438904dd41832e264e7cf70fd15c
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-21 11:04:44 +0900

    장바구니 state추가 및 Navbar에 전달

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,20 @@ import Routing from "./components/Routing/Routing";
 import { jwtDecode } from "jwt-decode";
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null); //현재 로그인 중인 유저 정보
+  const [cart, setCart] = useState([]); //장바구니
 
   //실행 시 로컬 스토리지에서 토큰 가져온다
   useEffect(() => {
     try {
       const jwt = localStorage.getItem("token"); //로컬스토리지에서 토큰가져옴
+
       const jwtUser = jwtDecode(jwt); //jwtDecode를 사용해서 가져온 토큰을 푼다.
-      //console.log(jwtUser); //유저 정보를 콘솔에 출력한다.
+      //console.log(jwtUser); //유저 정보를 콘솔에 출력한다
+
+      //현재 시간과 토큰 종료 시간을 비교해서 만료된 토큰은 삭제한다
       if (Date.now() >= jwtUser.exp * 1000) {
-        localStorage.removeItem("token"); //먄약 유효기간 지나면 삭제
+        localStorage.removeItem("token");
         location.reload();
       } else {
         setUser(jwtUser); // 유저 정보 User에 저장
@@ -22,10 +26,10 @@ function App() {
     } catch (error) {}
   }, []);
 
-  //console.log(user);
+  console.log(user);
   return (
     <div className="app">
-      <Navbar user={user} />
+      <Navbar user={user} cartCount={cart.length} />
       <main>
         <Routing />
       </main>
