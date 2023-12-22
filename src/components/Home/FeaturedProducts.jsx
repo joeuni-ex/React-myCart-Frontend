af011f5491e355c3ff0637cfc47734d64feae7aa
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-22 10:38:51 +0900

    useData를 사용해서 주요 제품 가져오기

--- src/components/Home/FeaturedProducts.jsx
+++ src/components/Home/FeaturedProducts.jsx
@@ -1,15 +1,20 @@
+import useData from "../../Hook/useData";
 import ProductCard from "../Products/ProductCard";
+import ProductCardSkeleton from "../Products/ProductCardSkeleton";
 import "./FeaturedProducts.css";
 
 const FeaturedProducts = () => {
+  const { data: featured, error, isLoading } = useData("/products/featured");
+  const skeletons = [1, 2, 3]; // 로딩 중
   return (
     <section className="featured_products">
       <h2>주요제품</h2>
 
       <div className="align_center featured_products_list">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {error && <em className="form_error">{error}</em>}
+        {isLoading && skeletons.map((n) => <ProductCardSkeleton key={n} />)}
+        {featured &&
+          featured.map((product) => <ProductCard product={product} />)}
       </div>
     </section>
   );
